10de502032776423b7a444fb49bebca7b3e1fb6e
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-02-20 09:01:26 +0300

    change template sets

--- django_library/settings.py
+++ django_library/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
